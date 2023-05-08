8a6cff20f6a8f1c2a13b5bc6af3ed76aec4e3c58
Abi0l9 <cryptog3ms@gmail.com>
2023-05-08 15:41:19 +0100

    created user model

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const schema = Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  fullName: {
+    type: String,
+    default: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: String,
+  city: String,
+  state: String,
+  country: String,
+  dob: String,
+  passwordHash: String,
+  gender: String,
+  occupation: String,
+  resume: String,
+  applications: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Job",
+    },
+  ],
+  feed: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Feed",
+    },
+  ],
+});
+
+schema.plugin(uniqueValidator);
+
+schema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = model("User", schema);
+module.exports = User;
